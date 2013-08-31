150be44b73357ce5ca6357f3185b6ad3a139723b
rgiordan <rgiordan@gmail.com>
2013-08-30 17:04:13 -0700

    Make some small changes to clean up.

--- everett_analysis.R
+++ everett_analysis.R
@@ -6,7 +6,7 @@ library(ggplot2)
 source("./everett_analysis_lib.R")
 options(warn=1)
 
-d.original <- read.delim("Desktop/everett_raw.txt", sep="")
+d.original <- read.delim("./everett_raw.txt", sep="")
 
 # The lower limit of the pareto distribution can't be larger than
 # the upper bound of the first bucket
@@ -15,8 +15,10 @@ x.min.upper <- d.original[1, "Max"]
 breaks <- d.original[, "Max"][-nrow(d.original)]  # Exclude the last row from the breaks
 counts <- d.original[, "Companies.n"]
 
-# The number of differenet pareto distributions
+# The number of different pareto distributions.  If this is 2, that means
+# the distribution can have two different power laws.
 regions <- 2
+
 
 UnpackPar <- function(par) {
   #x.min <- LogitFunction(par[1:regions], logit.max=x.min.upper)
@@ -24,6 +26,7 @@ UnpackPar <- function(par) {
   alpha <- ExpFunction(par[(regions + 1):(2 * regions)])
   return(list(x.min=x.min, alpha=alpha))
 }
+
 
 # Par should be the transformed parameters [ x.min, alpha ]
 # (the transformed parameters should be able to take any value on the
@@ -40,11 +43,6 @@ LikForOptim <- function(par) {
 
 
 # Starting parameters
-#par.0 <- c(InvLogitFunction(0.5, logit.max=x.min.upper),
-#           InvExpFunction(0.01))
-#par.0 <- c(InvExpFunction(0.5), InvExpFunction(1.5 * mean(d[["Max"]])),
-#           InvExpFunction(0.1), InvExpFunction(0.5))
-
 par.0 <- c(InvExpFunction(0.27), InvExpFunction(90),
            InvExpFunction(0.25), InvExpFunction(1.6))
 
@@ -73,37 +71,29 @@ for (b in 1:B) {
   print(x.min <- res.par$x.min)
   bootstrap.results[b,] <- c(b, res.par$alpha, res.par$x.min)
 }
-save(bootstrap.results, par.0, d.original, file="bootstrap_results.Rdata")
-
-
-boot.means <- rep(NA, B)
-for (b in 1:B) {
-  print(b)
-  alpha <- bootstrap.results[b, 2:3]
-  x.min <- bootstrap.results[b, 4:5]
-  boot.means[b] <- DrawPiecewiseParetoMoment(1e4, alpha, x.min, MomentFun=function(x) { x })
-}
+# Uncomment to save automatically when sourcing
+#save(bootstrap.results, par.0, d.original, file="bootstrap_results.Rdata")
 
 
 # Simulate data with the fitted parameters
-#sim <- DrawPareto(sum(d[,"Companies.n"]), x.min=x.min, alpha=alpha)
-#d$Simulated.Companies.n <- as.numeric(table(cut(sim, breaks=c(0, breaks, Inf))))
-
 d <- d.original
 sim <- DrawPiecewisePareto(n=sum(d[,"Companies.n"]), x.min=x.min, alpha=alpha)
 d$Simulated.Companies.n <- as.numeric(table(cut(sim, breaks=c(0, breaks, Inf))))
 
+# Compare the fitted and raw distributions
 ggplot(d, aes(x=log10(Avg))) + 
   geom_line(aes(y=log10(Companies.n), color="Actual"), lwd=2) +
   geom_line(aes(y=log10(Simulated.Companies.n), color="Sim"),lwd=2) +
   geom_vline(aes(xintercept=log10(x.min)), lwd=1)
 
 
+# Draw a big, uncensored simulated data set for moment computation.
 big.sim <- DrawPiecewisePareto(n=1e7, x.min=x.min, alpha=alpha)
 sd(big.sim)
 mean(big.sim)
 
 
+# Calculate the value of exposure to this risk.
 wealth <- 3 # This many million in wealth to start
 gamma <- 2  # CRRA Coefficient
 scale <- 0.01   # The percent of the exit value you actually get
